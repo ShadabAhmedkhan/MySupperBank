9c9ac6261b2f08a2dab626923bf5887289365cab
shadabahmedkhan <shadab33@hotmail.com>
2020-11-07 19:14:38 +0500

    Oriented Programming - Methods and Exceptions

--- BankAccount.cs
+++ BankAccount.cs
@@ -8,17 +8,51 @@ namespace MySupperBank
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        //public decimal Balance { get; }
+
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+        private List<Transaction> allTransactions = new List<Transaction>();
+        private static int accountNumberSeed = 1234567890;
         public BankAccount(string name,decimal initialBalance) {
             this.Owner = name;
-            this.Balance = initialBalance;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
-        public void MakeDeposite(decimal amount,DateTime date,string note) { 
-
-        }
-        public void MakeWithdrawel(decimal amount, DateTime date, string note)
+        public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
 
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ namespace MySupperBank
         static void Main(string[] args)
         {
             var account = new BankAccount("Shadab",10000);
-            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Number}");
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
         }
     }
 }
